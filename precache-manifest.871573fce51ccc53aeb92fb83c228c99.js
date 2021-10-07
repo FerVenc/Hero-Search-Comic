159c029e5f11c00f8a93e9e881405133aec6c0ca
FerVenc <fernandovc@outlook.com>
2021-10-07 12:36:07 -0500

    Updates

---
+++ precache-manifest.871573fce51ccc53aeb92fb83c228c99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b01c1a4dcd56b4b303f2aebf106b4342",
+    "url": "/Hero-Search-Comic/index.html"
+  },
+  {
+    "revision": "2f9f895c7b0010a38d43",
+    "url": "/Hero-Search-Comic/static/js/2.0bbf8f6b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Hero-Search-Comic/static/js/2.0bbf8f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ed475128c8e3c0e25b",
+    "url": "/Hero-Search-Comic/static/js/main.43b30f8d.chunk.js"
+  },
+  {
+    "revision": "db0521976d709dea7145",
+    "url": "/Hero-Search-Comic/static/js/runtime-main.ade89721.js"
+  }
+]);
